b1baa262f49f0fb1de4a9f624e831794a64b9c32
Gaston Fernandez <gastyfp@gmail.com>
2022-10-11 22:08:25 -0300

    changing an anchor to link 4 deploy

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import pokeImage from '../../assets/pokemonPNG.png'
 import backgroundImg from '../../assets/background.gif'
 import './Landing.scss'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
   return (
       <div className='container'>
         <img className='logo' src={pokeImage} alt="pokeIMAGE" />
-        <a href="http://localhost:3000/pokemons"><button className='landing-btn'> Ingresar</button></a>
+        <Link to={"/pokemons"}><button className='landing-btn'> Ingresar</button></Link>
         <img className='background' src={backgroundImg} alt="BACKGROUND" />
     </div>
   )
