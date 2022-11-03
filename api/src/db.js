d9f97fd703b387315cfbedddad177d062ba8f45f
Gaston Fernandez <gastyfp@gmail.com>
2022-11-03 12:06:05 -0300

    changes

--- api/src/db.js
+++ api/src/db.js
@@ -3,11 +3,11 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,PORT
 } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`
-const sequelize = new Sequelize(`postgresql://postgres:biqrTJd8SbGowiemTlkf@containers-us-west-53.railway.app:7691/railway`, {
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/:${PORT}/railway`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
